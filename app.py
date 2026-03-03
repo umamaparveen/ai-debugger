6dfbb0b8a795a9730544b0f11b90a28bf3923457
Umama Parveen <umamaparveen8@gmail.com>
2026-03-03 21:18:59 +0530

    Add run_code function

--- app.py
+++ app.py
@@ -7,6 +7,33 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+def run_code(code, language):
+
+    try:
+        # ================= PYTHON =================
+        if language == "python":
+            try:
+                result = subprocess.run(
+                    ["python", "-c", code],
+                    capture_output=True,
+                    text=True,
+                    timeout=5   # prevents infinite execution
+                )
+
+                # If error exists
+                if result.stderr:
+                    match = re.search(r'line (\d+)', result.stderr)
+                    if match:
+                        line = match.group(1)
+                        return f"❌ Python Error at line {line}\n\n{result.stderr}"
+
+                    return "❌ Python Error:\n" + result.stderr
+
+                return "✅ No errors found!\n\nOutput:\n" + result.stdout
+
+            except subprocess.TimeoutExpired:
+                return "⏱ Execution stopped: Code took too long or waiting for input."
+
 @app.route("/debug", methods=["POST"])
 def debug():
     try:
